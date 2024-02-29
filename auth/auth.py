d1d08df9883ff21768b2af9705aa6a45e985150c
Hadi Abtinfar <hadi.abtinfar@gmail.com>
2024-02-29 15:25:02 +0000

    blueprint and flask-restful

--- auth/auth.py
+++ auth/auth.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask-restful import Api
 from auth.config import Config
+
+apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
+apiv1 = Api(apiv1_bp)
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
